67599fee8850d9bb499063450b295b6f2f31153c
miguelsantanadv <142845053+miguelsantanadv@users.noreply.github.com>
2023-10-27 00:28:03 -0300

    Update Carrinho.java

--- AV1/Carrinho.java
+++ AV1/Carrinho.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class Carrinho {
     private int idCarrinho;
-    private ArrayList<Itens> itens;
+    ArrayList<Itens> itens;
+    private double total;
 
-    public Carrinho(int idCarrinho, ArrayList<Itens> itens) {
+    public Carrinho(int idCarrinho, ArrayList<Itens> itens, double total) {
         super();
         this.idCarrinho = idCarrinho;
-        this.itens =  new ArrayList();
+        this.itens =  new ArrayList<Itens>();
+        this.total = total;
     }
 
     public int getIdCarrinho() {
@@ -19,4 +21,20 @@ public class Carrinho {
       public void setIdCarrinho(int idCarrinho) {
         this.idCarrinho = idCarrinho;
       }
+      
+      public ArrayList<Itens> getItens() {
+          return itens;
+      }
+
+      public void adicionarItem(Itens item) {
+          itens.add(item);
+      }
+      
+      public double getTotal() {
+          return total;
+        }
+      
+        public void setTotal(double total) {
+          this.total = total;
+        }
 }
